a04dec79e0438c4e39f2d89af841ab9c0d9ac13d
Dawid Pałuska <dawidtychy5@gmail.com>
2025-05-29 01:47:51 +0200

    poprawki, usunięte zbędne rzeczy

--- datawedge-MAUI-SampleApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace datawedge_MAUI_SampleApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
